96d299bcd25020b4221e9e46dbf39683a6046b45
yujinju <jinju201118@gmail.com>
2023-06-26 22:14:53 +0900

    P1546

--- P1546.java
+++ P1546.java
@@ -5,17 +5,14 @@ public class P1546 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
-		int A[] = new int[N];
-		for(int i=0 ; i<N ; i++) {
-			A[i] = sc.nextInt();
-		}
 		
 		int sum = 0;
 		int mNum = 0;
 		for(int i=0 ; i<N ; i++) {
-			sum += A[i];
-			if(A[i] > mNum) {
-				mNum = A[i];
+			int temp = sc.nextInt();
+			sum += temp;
+			if(temp > mNum) {
+				mNum = temp;
 			}
 		}
 		
